b9098569df13795d91fd932c49248c22bc359a5a
Jonathan Ho <hojonathanho@gmail.com>
2015-09-05 14:15:09 -0700

    print openblas build

--- 4build/download_and_build_openblas.py
+++ 4build/download_and_build_openblas.py
@@ -21,4 +21,4 @@ call_and_print("mkdir -p {unpack_dir} && tar -xf {fname} --directory {unpack_dir
     fname=fname,unpack_dir=unpack_dir))
 os.chdir(unpack_dir)
 print "Compiling OpenBLAS...this will take a minute or so"
-call_and_print("make -j ONLY_CBLAS=1 NO_LAPACK=1 NO_LAPACKE=1 USE_THREAD=0 USE_OPENMP=0 &> compile_output.txt")
+call_and_print("make -j ONLY_CBLAS=1 NO_LAPACK=1 NO_LAPACKE=1 USE_THREAD=0 USE_OPENMP=0")
